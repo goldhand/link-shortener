2406d36bb57d0c9030d6ebca2cccb6ca2f358217
goldhand <will@django.nu>
2015-09-26 12:45:13 -0400

    adds link key to Link model for shorter link url

--- links/models.py
+++ links/models.py
@@ -4,6 +4,22 @@ from django.db import models
 from django.conf import settings
 from django.core.urlresolvers import reverse
 
+from .settings import DEFAULT_LINK_LENGTH
+
+
+def set_link_key():
+    return uuid.uuid4().hex.title()[:DEFAULT_LINK_LENGTH]
+
+
+def get_unique_link_key(link):
+    '''
+    make sure link that is returned is unique
+    '''
+    link_key = link.link_key
+    while Link.objects.exclude(id=link.id).filter(link_key=link_key):
+        link_key = set_link_key()
+    return link_key
+
 
 class Link(models.Model):
     '''
@@ -14,17 +30,23 @@ class Link(models.Model):
                                verbose_name='URL')
     name = models.CharField(max_length=255,
                             help_text='Title of page link is going to')
-
     owner = models.ForeignKey(settings.AUTH_USER_MODEL)
+    link_key = models.SlugField(unique=True, max_length=100,
+                                default=set_link_key,
+                                help_text='Custom shortend version of link')
 
     def __unicode__(self):
         return u'{}'.format(self.name)
 
-    def get_absolute_url(self):
-        return reverse('links:detail', self.id)
+    def __init__(self, *args, **kwargs):
+        '''
+        assert link_key is unique when object is initialized
+        '''
+        super(Link, self).__init__(*args, **kwargs)
+        self.link_key = get_unique_link_key(self)
 
     def shortend_url(self):
         '''
         link that request should be routed to
         '''
-        return reverse('links:shorten', self.id)
+        return reverse('links:shorten', kwargs={'link_key': self.link_key})
