dbf23cace76df237dd1715d21d270202775f088b
goldhand <will@django.nu>
2015-09-26 13:33:25 -0400

    connects link update and shortens redirect view

--- links/urls.py
+++ links/urls.py
@@ -11,6 +11,8 @@ urlpatterns = [
     url(r'^create/$', view=views.LinkCreate.as_view(), name='create'),
     url(r'^delete/(?P<pk>[\w.@+-]+)$', view=views.LinkDelete.as_view(),
         name='delete'),
-    url(r'^(?P<pk>[\w.@+-]+)/$', view=views.LinkRedirect.as_view(),
+    url(r'^update/(?P<pk>[\w.@+-]+)$', view=views.LinkUpdate.as_view(),
+        name='update'),
+    url(r'^(?P<link_key>[\w.@+-]+)/$', view=views.LinkRedirect.as_view(),
         name='shorten'),
 ]
--- links/views.py
+++ links/views.py
@@ -1,7 +1,7 @@
-from django.views.generic import CreateView, ListView, RedirectView, DeleteView
-from django.views.generic.detail import SingleObjectMixin
-
+from django.views.generic import (CreateView, ListView, RedirectView,
+                                  DeleteView, UpdateView)
 from django.core.urlresolvers import reverse
+from django.shortcuts import get_object_or_404
 
 from braces.views import LoginRequiredMixin
 
@@ -23,6 +23,11 @@ class LinkCreate(LoginRequiredMixin, CreateView):
         form.instance.owner = self.request.user
         return super(LinkCreate, self).form_valid(form)
 
+    def get_context_data(self, *args, **kwargs):
+        context = super(LinkCreate, self).get_context_data(*args, **kwargs)
+        context['action_url'] = reverse('links:create')
+        return context
+
 
 class LinkList(LoginRequiredMixin, ListView):
     '''
@@ -43,17 +48,16 @@ class LinkList(LoginRequiredMixin, ListView):
         return context
 
 
-class LinkRedirect(SingleObjectMixin, RedirectView):
+class LinkRedirect(RedirectView):
     '''
     redirects a request to the link base_url
     '''
 
     permanent = False
-    model = Link
-    pk_url_kwarg = 'pk'  # TODO: Does SingleObjectMixin need this?
 
-    def get_redirect_url(self, pk):
-        return self.get_object().base_url
+    def get_redirect_url(self, *args, **kwargs):
+        link = get_object_or_404(Link, link_key=kwargs['link_key'])
+        return link.base_url
 
 
 class LinkDelete(LoginRequiredMixin, DeleteView):
@@ -71,3 +75,24 @@ class LinkDelete(LoginRequiredMixin, DeleteView):
 
     def get_success_url(self):
         return reverse('links:list')
+
+
+class LinkUpdate(LoginRequiredMixin, UpdateView):
+    '''
+    Updates a link
+    '''
+
+    model = Link
+    fields = ['base_url', 'name', 'link_key']
+
+    def get_queryset(self):
+        return Link.objects.filter(owner=self.request.user)
+
+    def get_success_url(self):
+        return reverse('links:list')
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(LinkUpdate, self).get_context_data(*args, **kwargs)
+        context['action_url'] = reverse('links:update',
+                                        kwargs={'pk': self.get_object().pk})
+        return context
