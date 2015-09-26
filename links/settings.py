67bad6b42bce678bb7258d7f963ee1bf5536059d
goldhand <will@django.nu>
2015-09-26 13:25:25 -0400

    adds link_key tests

---
+++ links/settings.py
@@ -0,0 +1,4 @@
+from django.conf import settings
+
+
+DEFAULT_LINK_LENGTH = getattr(settings, 'DEFAULT_LINK_LENGTH', 5)
--- links/tests.py
+++ links/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Link
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class LinkTestCase(TestCase):
+    def setUp(self):
+        user = User.objects.create(username='testuser')
+        Link.objects.create(base_url='https://google.com',
+                                     owner=user, name='google')
+
+    def test_key_unique_on_init(self):
+        g_link = Link.objects.get(name='google')
+        user = User.objects.get(username='testuser')
+        fb_link = Link(base_url='https://facebook.com',
+                       owner=user, name='facebook',
+                       link_key=g_link.link_key)
+        self.assertNotEqual(fb_link.link_key, g_link.link_key)
